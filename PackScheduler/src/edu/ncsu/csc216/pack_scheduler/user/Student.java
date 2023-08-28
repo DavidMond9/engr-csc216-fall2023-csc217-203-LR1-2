065fb1cbbfa58e799110186886bc7bad89bb55fb
dmmond <dmmond@ncsu.edu>
2023-08-28 19:31:04 -0400

    Implemented the Student State.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
@@ -3,6 +3,14 @@ package edu.ncsu.csc216.pack_scheduler.user;
 public class Student {
 	
 	public static final int MAX_CREDITS = 18;
+	private String firstName;
+	private String lastName;
+	private String id;
+	private String email;
+	private String password;
+	private int maxCredits;
+	
+	
 
 	public Student(String firstName, String lastName, String id, String email, String hashPW, int maxCredits) {
 		// TODO Auto-generated constructor stub
