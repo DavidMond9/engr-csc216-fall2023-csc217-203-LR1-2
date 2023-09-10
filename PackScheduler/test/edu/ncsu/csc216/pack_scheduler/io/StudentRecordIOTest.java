93c3b218a6f331a4eb6530c6dcb82a3c8e7ad357
Sean Van Acker <smvanack@ncsu.edu>
2023-09-10 15:31:16 -0400

    [Implementation] Added StudentRecordIOTest Skeleton

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.io;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for StudentRecordIO Object
+ * 
+ * @author Sean Van Acker
+ * @author 
+ */
+class StudentRecordIOTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.io.StudentRecordIO#readStudentRecords(java.lang.String)}.
+	 */
+	@Test
+	void testReadStudentRecords() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.io.StudentRecordIO#writeStudentRecords(java.lang.String, java.util.ArrayList)}.
+	 */
+	@Test
+	void testWriteStudentRecords() {
+		fail("Not yet implemented");
+	}
+
+}
