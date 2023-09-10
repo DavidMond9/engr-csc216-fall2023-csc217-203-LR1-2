31de356dec3ce9aa5110d09b1ab1814939e7fcf7
Sean Van Acker <smvanack@ncsu.edu>
2023-09-10 17:46:10 -0400

    [Implementation] Added Test cases for readStudentRecords and writeStudentRecords.

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -4,15 +4,20 @@
 package edu.ncsu.csc216.pack_scheduler.io;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Scanner;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import edu.ncsu.csc216.pack_scheduler.user.Student;
 
 /**
  * Tests for StudentRecordIO Object
@@ -22,6 +27,11 @@ import org.junit.jupiter.api.Test;
  */
 class StudentRecordIOTest {
 
+	/** Valid student records */
+	private final String validTestFile = "test-files/student_records.txt";
+	/** Invalid student records */
+	private final String invalidTestFile = "test-files/invalid_student_records.txt";
+	
 	private String validStudent0 = "Zahir,King,zking,orci.Donec@ametmassaQuisque.com,pw,15";
 	private String validStudent1 = "Cassandra,Schwartz,cschwartz,semper@imperdietornare.co.uk,pw,4";
 	private String validStudent2 = "Shannon,Hansen,shansen,convallis.est.vitae@arcu.ca,pw,14";
@@ -91,15 +101,80 @@ class StudentRecordIOTest {
 	 */
 	@Test
 	void testReadStudentRecords() {
-		fail("Not yet implemented");
+		try {
+			ArrayList<Student> s = StudentRecordIO.readStudentRecords(validTestFile);
+			assertEquals(10, s.size());
+			
+			for (int i = 0; i < validStudents.length; i++) {
+				assertEquals(validStudents[i], s.get(i).toString());
+			}
+		} catch (FileNotFoundException e) {
+			fail("Unexpected error reading " + validTestFile);
+		}
 	}
 
+	
+	/**
+	 * Tests readStudentRecords() with invalid test file
+	 */
+	@Test
+	public void testReadInvalidStudentRecords() {
+		ArrayList<Student> s;
+		try {
+			s = StudentRecordIO.readStudentRecords(invalidTestFile);
+			assertEquals(0, s.size());
+		} catch (FileNotFoundException e) {
+			fail("Unexpected FileNotFoundException");
+		}
+	}
+	
+//	/**
+//	 * Tests readStudentRecords() for a file that does not exist.
+//	 */
+//	@Test
+//	public void testReadRecordsFileDoesNotExist() {
+//		ArrayList<Student> s;
+//		Exception e = assertThrows(FileNotFoundException.class,
+//					() -> (s = StudentRecordIO.readStudentRecords("test-files/not_a_real_file.txt")));
+//	}
+	
 	/**
 	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.io.StudentRecordIO#writeStudentRecords(java.lang.String, java.util.ArrayList)}.
 	 */
 	@Test
 	void testWriteStudentRecords() {
-		fail("Not yet implemented");
+		ArrayList<Student> students = new ArrayList<Student>();
+		students.add(new Student("Zahir", "King", "zking", "orci.Donec@ametmassaQuisque.com", hashPW, 15));
+		students.add(new Student("Cassandra","Schwartz","cschwartz","semper@imperdietornare.co.uk",hashPW,4));
+		students.add(new Student("Shannon","Hansen","shansen","convallis.est.vitae@arcu.ca",hashPW,14));
+		students.add(new Student("Demetrius","Austin","daustin","Curabitur.egestas.nunc@placeratorcilacus.co.uk",hashPW,18));
+		students.add(new Student("Raymond","Brennan","rbrennan","litora.torquent@pellentesquemassalobortis.ca",hashPW,12));
+		students.add(new Student("Emerald","Frost","efrost","adipiscing@acipsumPhasellus.edu",hashPW,3));
+		students.add(new Student("Lane","Berg","lberg","sociis@non.org",hashPW,14));
+		students.add(new Student("Griffith","Stone","gstone","porta@magnamalesuadavel.net",hashPW,17));
+		students.add(new Student("Althea","Hicks","ahicks","Phasellus.dapibus@luctusfelis.com",hashPW,11));
+		students.add(new Student("Dylan","Nolan","dnolan","placerat.Cras.dictum@dictum.net",hashPW,5));
+		try {
+			StudentRecordIO.writeStudentRecords("test-files/actual_student_records.txt", students);
+		} catch (IOException e) {
+			fail("Cannot write to student records file");
+		}
+		
+		checkFiles("test-files/expected_full_student_records.txt", "test-files/actual_student_records.txt");
+	}
+	
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.io.StudentRecordIO#writeStudentRecords(java.lang.String, java.util.ArrayList)}.
+	 */
+	@Test
+	public void testWriteStudentRecordsNoPermissions() {
+		ArrayList<Student> students = new ArrayList<Student>();
+		students.add(new Student("Zahir", "King", "zking", "orci.Donec@ametmassaQuisque.com", hashPW, 15));
+
+		
+		Exception exception = assertThrows(IOException.class, 
+				() -> StudentRecordIO.writeStudentRecords("/home/sesmith5/actual_student_records.txt", students));
+		assertEquals("/home/sesmith5/actual_student_records.txt (No such file or directory)", exception.getMessage());
 	}
 
 }
