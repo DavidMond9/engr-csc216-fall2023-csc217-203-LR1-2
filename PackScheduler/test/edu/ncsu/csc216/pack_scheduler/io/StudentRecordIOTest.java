caa09e95a7ea912693db7b097f6f1063c6cac7fe
dmmond <dmmond@ncsu.edu>
2023-09-10 15:43:47 -0400

    [Implementation] Implemented checkFiles helper method.

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -5,6 +5,10 @@ package edu.ncsu.csc216.pack_scheduler.io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +19,36 @@ import org.junit.jupiter.api.Test;
  * @author 
  */
 class StudentRecordIOTest {
+	
+	
+	/**
+	 * Helper method to compare two files for the same contents
+	 * @param expFile expected output
+	 * @param actFile actual output
+	 */
+	private void checkFiles(String expFile, String actFile) {
+		try (Scanner expScanner = new Scanner(new FileInputStream(expFile));
+			 Scanner actScanner = new Scanner(new FileInputStream(actFile));) {
+			
+			while (expScanner.hasNextLine()  && actScanner.hasNextLine()) {
+				String exp = expScanner.nextLine();
+				String act = actScanner.nextLine();
+				assertEquals(exp, act, "Expected: " + exp + " Actual: " + act); 
+				//The third argument helps with debugging!
+			}
+			if (expScanner.hasNextLine()) {
+				fail("The expected results expect another line " + expScanner.nextLine());
+			}
+			if (actScanner.hasNextLine()) {
+				fail("The actual results has an extra, unexpected line: " + actScanner.nextLine());
+			}
+			
+			expScanner.close();
+			actScanner.close();
+		} catch (IOException e) {
+			fail("Error reading files.");
+		}
+	}
 
 	/**
 	 * @throws java.lang.Exception
